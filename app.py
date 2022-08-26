ea317a3326d744fdc1dcd06a49aed41cff48eb43
dkalia <dkalia@users.noreply.github.com>
2022-08-26 15:47:29 -0400

    Added new components

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ st.title("Demo Dashboard")
 st.write("Example dashboard to Marquee class for Queen's University")
 
 
+categories = ['a', 'b', 'c']
+st.multiselect("pick an option", categories)
+
+st.sidebar.button("Click me!")
+
 
