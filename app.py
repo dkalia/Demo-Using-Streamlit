0081e1838a5cc373dfc08bc26706576c92572c37
dkalia <dkalia@users.noreply.github.com>
2022-08-26 15:52:30 -0400

    Added IPO

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import pandas as pd
 st.title("Demo Dashboard")
 st.write("Example dashboard to Marquee class for Queen's University")
 
-
+#st.write is similar to print() in python
 categories = ['a', 'b', 'c']
 st.multiselect("pick an option", categories)
 
 st.sidebar.button("Click me!")
 
+url = "https://www.iposcoop.com/last-100-ipos/"
+df = pd.read_html(url)[0]
+
+st.write(df)
+
+st.dataframe(df)
 
